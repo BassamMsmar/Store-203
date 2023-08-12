7df3a34cfb7bd7a41a0fdde8ed790f2c6d37420d
BassamMsmar <bassammsmar@gmail.com>
2023-08-12 14:23:13 +0300

    add autherzitons to product list

--- store/views.py
+++ store/views.py
@@ -24,33 +24,40 @@ def product_details(request, pk):
 
 
 def add_product(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
+    if request.user.is_authenticated and request.user.is_superuser:
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES)
 
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-        
-    else:
-        form = ProductForm()
+            if form.is_valid():
+                form.save()
+                return redirect('login')
 
-    return render(request, 'store/product_add.html',{'form':form})
+        else:
+            form = ProductForm()
+
+        return render(request, 'store/product_add.html',{'form':form})
+    else: 
+        return redirect('products')
+
 
 
 
 def edit_product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
+    if request.user.is_authenticated and request.user.is_superuser:
+        product = get_object_or_404(Product, pk=pk)
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES)
 
-        if form.is_valid():
-            form.save()
-            return redirect('products')
-        
-    else:
-        form = ProductForm(instance=product)
-        
-    return render(request, 'store/product_add.html',{'form':form})
+            if form.is_valid():
+                form.save()
+                return redirect('products')
+
+        else:
+            form = ProductForm(instance=product)
+
+        return render(request, 'store/product_add.html',{'form':form})
+    else : 
+        return redirect('products')
 
 
 def product_delete(request, pk):
@@ -60,6 +67,7 @@ def product_delete(request, pk):
 
 
 class CreateCategory(CreateView):
+    
     model = Category
     fields = ['image', 'name']
     success_url = '/products'
